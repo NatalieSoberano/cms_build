821430de721726150992d6d774fb7a41611bc69e
Natalie Soberano <nataliesoberano@Natalies-MBP-4.fc.ca>
2020-01-20 16:37:21 -0500

    second class changes

---
+++ load.php
@@ -0,0 +1,10 @@
+<?php 
+define('ABSPATH', __DIR__); // absolute path determins the route directory
+define('ADMIN_PATH', ABSPATH. '/admin'); //admin folder 
+define('ADMIN_SCRIPT_PATH', ADMIN_PATH. '/scripts'); //admin script path 
+
+
+
+ini_set('display errors', 1);
+require_once ABSPATH.'/config/database.php';
+require_once ADMIN_SCRIPT_PATH.'/read.php';
